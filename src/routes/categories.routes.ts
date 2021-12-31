a169332bfc551d0884c305db8f854b540c0454b9
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-12-31 17:11:14 -0400

    creating routes and use them in server | yarn add uudi

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = []
+
+// POST ROUTE
+categoriesRoutes.post("/", (request, response) => {
+    const {name, description } = request.body;
+
+    const category = {
+        id: uuidv4(),
+        name,
+        description,
+        created_at: new Date()
+    };
+
+    categories.push(category)
+
+    return response.status(201).json({category})
+})
+
+export {categoriesRoutes}
