6a90ca84fbdffc34951a443ea00f5857ed810529
Thiago Linchin <dev.thiagolinch@gmail.com>
2022-01-03 23:31:30 -0400

    Create CategoryRepository Methods: create and list | Refactoring routes files to use Repositoy

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,30 +1,27 @@
 import { Router } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
 
 
 // POST ROUTE
 categoriesRoutes.post("/", (request, response) => {
     const {name, description } = request.body;
 
-    const category = new Category();
+    categoriesRepository.create({name, description})
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date(),
-    });
-
-    categories.push(category)
-
-    return response.status(201).json({category})
+    return response.status(201).send();
 })
 
 // GET ALL CATEGORIES ROUTE
 categoriesRoutes.get("/", (req, res) => {
-    return res.json({categories})
+    const categories = categoriesRepository.list();
+
+    return res.json(categories)
 })
 
 export {categoriesRoutes}
